c3be26e8d68e40a6e12d7d547de496ea416e8cb3
heojoung2 <heojoung2@gmail.com>
2017-06-28 18:58:06 +0900

    linked_list

---
+++ linked_list/singly_linked_list/main.cpp
@@ -0,0 +1,19 @@
+#include "singly_linked_list.h"
+
+int main()
+{
+	Linked_list linked_list;
+
+	linked_list.Add(0, 1);
+	linked_list.Add(1, 2);
+	linked_list.Add(2, 3);
+	linked_list.Display();
+
+	linked_list.Delete(1);
+	linked_list.Display();
+
+	cout<<"search(1) : "<<linked_list.Search(1) << endl;
+	cout <<"length : " <<linked_list.Get_length() << endl;
+	linked_list.Free();
+	return 0;
+}
