84490d2f7fb501ee9406d3572f8ccba72d53bc45
heojoung2 <heojoung2@gmail.com>
2018-01-21 15:33:06 +0900

    Breadth-first search

---
+++ Algorithm/BFS/main.cpp
@@ -0,0 +1,27 @@
+#include "graph_list.h"
+
+int main()
+{
+	int size = 8;
+	Graph graph(size);
+	graph.Make_graph();
+	graph.Add_node(0);
+	graph.Add_node(1);
+	graph.Add_node(2);
+	graph.Add_node(3);
+	graph.Add_node(4);
+	graph.Add_node(5);
+	graph.Add_node(6);
+	graph.Add_node(7);
+	graph.Add_edge(0, 1);
+	graph.Add_edge(0, 2);
+	graph.Add_edge(1, 3);
+	graph.Add_edge(1, 4);
+	graph.Add_edge(4, 5);
+	graph.Add_edge(2, 5);
+	graph.Add_edge(2, 6);
+	graph.Add_edge(3, 7);
+
+	graph.BFS();
+	return 0;
+}
